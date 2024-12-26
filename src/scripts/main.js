f685d0ef8b3d139bd80d1da33245f0b95ab70940
Eugene <greenteyn@gmail.com>
2024-12-26 21:15:24 +0500

    fix router

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,21 +1,19 @@
 const routes = [
-  { path: "/", page: "main" },
-  { path: "/login", page: "login" },
-  { path: "/registration", page: "registration" },
-  { path: "/profile", page: "profile" },
-  { path: "/chat", page: "chat" },
-  { path: "/error", page: "error" },
+  { path: "/", page: "/index.html" },
+  { path: "/login", page: "/src/pages/login/index.html" },
+  { path: "/registration", page: "/src/pages/registration/index.html" },
+  { path: "/profile", page: "/src/pages/profile/index.html" },
+  { path: "/chat", page: "/src/pages/chat/index.html" },
+  { path: "/error", page: "/src/pages/error/index.html" },
 ];
 
 const validateUrl = (currentUrl) => {
-  return routes.find((route) => route.path === currentUrl)
-    ? currentUrl
-    : "/error";
+  const pageObj = routes.find((route) => route.path === currentUrl);
+
+  return pageObj ? pageObj.page : "/src/pages/error/index.html";
 };
 
-const loadPage = (pageUrl) => {
-  const filePath = `/src/pages${pageUrl}/index.html`;
-
+const loadPage = (filePath) => {
   fetch(filePath)
     .then((response) => response.text())
     .then((text) => (document.getElementById("app").innerHTML = text));
@@ -24,9 +22,9 @@ const loadPage = (pageUrl) => {
 const router = () => {
   const currentLocation = location.pathname;
 
-  const pageUrl = validateUrl(currentLocation);
+  const pageFile = validateUrl(currentLocation);
 
-  loadPage(pageUrl);
+  loadPage(pageFile);
 };
 
 window.addEventListener("hashchange", router);
